808a1229c2d6b1afdc5f631f2eb609896e16f62e
srilatha22 <41733660+srilatha22@users.noreply.github.com>
2018-08-09 10:50:35 +0530

    Create Ideone5.java

---
+++ program/Ideone5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone5
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      int a=2,b=3;
+      double result=Math.pow(a,b);
+      System.out.println("Answer is:" +result);
+ 
+	}
+}
