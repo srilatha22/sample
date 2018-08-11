c8cec1152a65214a5683822b88c05c90dcb50bad
srilatha22 <41733660+srilatha22@users.noreply.github.com>
2018-08-11 14:29:18 +0530

    Create ideone33.java

---
+++ program/ideone33.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone33
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+String bar = " ba jfjf jjj j   ";
+String[] split = bar.split( " " );
+System.out.println( split.length );
+}
+}
