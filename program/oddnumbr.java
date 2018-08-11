12df41922f876a790a78cf62e9694c4062a1e870
srilatha22 <41733660+srilatha22@users.noreply.github.com>
2018-08-11 09:45:48 +0530

    Create oddnumbr.java

---
+++ program/oddnumbr.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class oddnumbr
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+
+        int low = 20, high = 50;
+
+        while (low < high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+                // condition for nonprime number
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag)
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+}
